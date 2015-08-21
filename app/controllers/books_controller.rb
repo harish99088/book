5940d6a6f5c0f71250149e9b6f2ce0bc3b228f3a
harish99088 <rapuruharish123@gmail.com>
2015-08-21 09:18:53 +0530

    added review and stylesheets

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,22 @@
  class BooksController < ApplicationController
  	before_action :find_book, only: [:show, :edit, :update, :destroy]
+ 	before_action :authenticate_user!, only: [:new, :edit]
 
 def index 
 	if params[:category].blank?
 	@books=Book.all.order("created_at DESC")
-else
+ else
 	@category_id=Category.find_by(name: params[:category]).id
 	@books=Book.where(:category_id => @category_id).order("created_at DESC")
-end
+   end
 
 end
 def show
+	if @book.reviews.blank?
+		@average_review = 0
+	else
+		@average_review = @book.reviews.average(:rating).round(2)
+	 end
 
 	end
 
